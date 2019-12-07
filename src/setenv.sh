f3d3bfdd4c916d2a061897a982b5fd922e49267d
sunnyfenng <1356851902@qq.com>
2019-12-07 18:17:59 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,8 +1,8 @@
-export GITACCOUNT=
+export GITACCOUNT=SunnyFenng
 
 export MYCLUSTER=
 export KUBECONFIG=
 
-export NAMESPACE=
-export EMAIL=
+export NAMESPACE=fengzhaoyang
+export EMAIL=1356851902@qq.com
 export INGRESS=
