cbdc103ff70353ead57c7ef2717661f1791eeb69
sunnyfenng <1356851902@qq.com>
2019-12-07 18:25:55 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,7 +1,7 @@
 export GITACCOUNT=SunnyFenng
 
 export MYCLUSTER=tektonknativebeijing40
-export KUBECONFIG=
+export KUBECONFIG=/home/zhaoyang/.bluemix/plugins/container-service/clusters/tektonknativebeijing40/kube-config-tok04-tektonknativebeijing40.yml
 
 export NAMESPACE=fengzhaoyang
 export EMAIL=1356851902@qq.com
